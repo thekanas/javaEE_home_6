d527d869be9ebd6a85b0d47b8e427cf8e5a5bcfc
Andrey S <thekanas@yandex.ru>
2023-04-04 23:52:27 +0300

    поправил стÐкод стаил

--- src/main/java/by/stolybko/web/servlet/fullName.java
+++ src/main/java/by/stolybko/web/servlet/fullName.java
@@ -6,9 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 import java.io.PrintWriter;
+
 @WebServlet("/fullName")
 public class fullName extends HttpServlet {
     @Override
--- src/main/java/by/stolybko/web/servlet/sendName.java
+++ src/main/java/by/stolybko/web/servlet/sendName.java
@@ -5,9 +5,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.BufferedReader;
 import java.io.IOException;
+
 @WebServlet("/sendName")
 public class sendName extends HttpServlet{
     @Override
